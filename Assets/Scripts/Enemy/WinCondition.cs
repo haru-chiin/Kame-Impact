f012b1c19e82838ab705878ed6b9d8b82ac18eb1
Haruuu <andarubaru@gmail.com>
2023-06-06 16:52:11 +0700

    Initial

---
+++ Assets/Scripts/Enemy/WinCondition.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    private RangedEnemy RangedEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/respawn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class respawn : MonoBehaviour
+{
+    /*  private Transform currentCheckpoint;
+      private Health playerHealth;
+      private UIMa
+      // Start is called before the first frame update
+      private void Awake()
+      {
+         playerHealth = GetComponent<Health>();
+      }
+
+      // Update is called once per frame
+  public void Respawn()
+      {
+
+      }*/
+    
+}
